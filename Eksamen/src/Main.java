2df5c4e438527e546a32c74233a3b437ad32ee7b
thenil123 <thni020@student.kristiania.no>
2026-01-13 11:23:39 +0100

    Delete Eksamen directory

--- Eksamen/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException, IOException {
-        new Program().run(); //Starter hele programmet
-    }
-}
--- Eksamen/src/Scrapyard.java
+++
@@ -1,9 +0,0 @@
-//Her komemrn record for skraphandler:
-public record Scrapyard(int id, String name, String address, String phoneNumber) {
-
-    @Override //Får frem detaljene om Scrapyard
-    public String toString() {
-
-        return String.format("ID: %d, Name: %s, Address: %s, Phone: %s", id, name, address, phoneNumber);
-    }
-}
